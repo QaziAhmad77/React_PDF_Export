3251b2e7b8ee0add1f68edf488fae75509d989cb
Qazi Ahmad <qaziahmad1877@gmail.com>
2024-02-01 15:59:51 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { jsPDF } from "jspdf";
-import "jspdf-autotable";
+import React, { useEffect, useState } from 'react';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
 
 const App = () => {
   const [data, setData] = useState([]);
-
+  console.log(data.products, 'data');
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
+    fetch('https://dummyjson.com/products')
       .then((res) => res.json())
       .then(async (data) => {
         setData(data);
@@ -14,18 +14,48 @@ const App = () => {
       .then((json) => console.log(json));
   }, []);
 
-  const exportPdf = async () => {
-    const doc = new jsPDF({ orientation: "landscape" });
-
-    doc.autoTable({
-      html: "#my-table",
-    });
-
-    doc.save("mypdf.pdf");
+  const createHeaders = (keys) => {
+    const result = [];
+    for (let key of keys) {
+      result.push({
+        id: key,
+        name: keys[key],
+        prompt: key,
+      });
+    }
+    return result;
   };
 
+  const exportPdf = async () => {
+    // const headers = createHeaders([
+    //   'id',
+    //   'title',
+    //   'brand',
+    //   'category',
+    //   'price',
+    //   'rating',
+    // ]);
+    const doc = new jsPDF({
+      orientation: 'landscape',
+      unit: 'px',
+      floatPrecision: 2,
+    });
+    // const tableData = data?.products?.map((row) => ({
+    //   ...row,
+    //   id: row.id,
+    //   price: row.price,
+    //   rating: row.rating,
+    // }));
+    // doc.table(1, 1, tableData, headers, { autoSize: true });
+
+    doc.autoTable({
+      html: '#my-table',
+    });
+    doc.setFont('Courier');
+    doc.save('mypdf.pdf');
+  };
   return (
-    <div style={{ padding: "30px" }}>
+    <div style={{ padding: '30px' }}>
       <button
         className="btn btn-primary float-end mt-2 mb-2"
         onClick={exportPdf}
@@ -34,7 +64,7 @@ const App = () => {
       </button>
       <h3>Table Data:</h3>
       <table className="table table-bordered" id="my-table">
-        <thead style={{ background: "yellow" }}>
+        <thead style={{ background: 'yellow' }}>
           <tr>
             <th scope="col">Id</th>
             <th scope="col">Title</th>
